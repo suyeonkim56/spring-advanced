c3f27828186f38b32fd63296464dfc81b2223a0a
suyeonkim <suyeonk56@gmail.com>
2025-02-24 22:25:03 +0900

    레벨 1-3 코드 개선 validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,9 +1,13 @@
 package org.example.expert.domain.user.dto.request;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 
 @Getter
 @NoArgsConstructor
@@ -12,6 +16,9 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
+
     @NotBlank
+    @Size(min = 8)
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\\\d).*$", message = "새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.")
     private String newPassword;
 }
