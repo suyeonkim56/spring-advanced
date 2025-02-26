1f682bfaae49e96f7ddd9a8fb438f15f858d7955
suyeonkim <suyeonk56@gmail.com>
2025-02-26 20:29:12 +0900

    LV3 테스트 코드 연습 1

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
@@ -23,6 +24,6 @@ class PasswordEncoderTest {
         boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
 
         // then
-        assertTrue(matches);
+        assertFalse(matches);
     }
 }
